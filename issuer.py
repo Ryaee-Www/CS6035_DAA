d887ff2b2f63a74878a8d03b5255e75b1fa98b64
Xwg <loloxwg@gmail.com>
2024-04-12 15:45:45 +0800

    Refactor key naming and add TPM class
    Renamed keys in `Issuer`, `Host`, and `Verifier` for clarity, changing 'signing_key', 'verifying_key' to 'ek' and 'aik'. Removed direct generation of host's signing key, now all keys are directly issued by `Issuer`. Also introduced a new `TPM` class mimicking `Host` class behavior, demonstrating flexible attestation source.

--- issuer.py
+++ issuer.py
@@ -1,12 +1,11 @@
-from ecdsa import SigningKey, VerifyingKey, SECP256k1
+from ecdsa import SigningKey, SECP256k1
 
 
 class Issuer:
     def __init__(self):
-        self.signing_key = SigningKey.generate(curve=SECP256k1)  # Generate a new ECDSA key pair
-        self.verifying_key = self.signing_key.get_verifying_key()
+        self.ek = SigningKey.generate(curve=SECP256k1)  # Generate a new ECDSA key pair
+        self.aik = self.ek.get_verifying_key()
 
     def issue(self):
         # Issue a private key to a host (in reality this should preferably be done securely)
-        host_signing_key = SigningKey.generate(curve=SECP256k1)
-        return host_signing_key, host_signing_key.get_verifying_key()
+        return self.ek, self.aik
